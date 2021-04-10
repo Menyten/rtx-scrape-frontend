003dbe93d361ba352e541691c330057ffd827d33
Joel Pedersen <joel.pedersen97@gmail.com>
2021-04-10 23:59:12 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const Card = styled.section`
   padding: 1rem;
   border: 2px solid #fff;
   border-radius: 25px;
+  align-self: flex-start;
 `;
 
 const CardHeader = styled.header`
@@ -36,21 +37,20 @@ const List = styled.ul``;
 
 const ListItem = styled.li`
   &:not(:last-child) {
-    margin-bottom: 1rem;
+    margin-bottom: 1.5rem;
   }
 `;
 
-const ProductContainer = styled.section`
-  display: flex;
-`;
-
-const ProductImage = styled.img`
-  max-width: 20%;
-`;
+const ProductContainer = styled.section``;
 
 const ProductTitle = styled.h4`
   color: #fff;
   font-weight: 500;
+  margin-bottom: 0.5rem;
+`;
+
+const ProductPrice = styled.p`
+  color: #fff;
   margin-bottom: 0.5rem;
 `;
 
@@ -60,10 +60,6 @@ const ProductStatus = styled.p`
 `;
 
 const ProductLink = styled.a``;
-
-const ProductContentWrapper = styled.div`
-  padding-left: 1rem;
-`;
 
 const renderProducts = (products) => {
   const companies = Object.keys(products);
@@ -78,24 +74,21 @@ const renderProducts = (products) => {
       <CardContent>
         <List>
           {products[company].map(
-            ({ productName, productInStock, productUrl, productImage }) => (
+            ({ productName, productInStock, productUrl, productPrice }) => (
               <ListItem key={productName}>
                 <ProductContainer>
-                  {/* <img src={productImage} alt="" /> */}
-                  <ProductImage src={productImage} />
-                  <ProductContentWrapper>
-                    <ProductTitle>{productName}</ProductTitle>
-                    <ProductStatus inStock={productInStock}>
-                      {productInStock ? "Finns i lager" : "Slut"}
-                    </ProductStatus>
-                    <ProductLink
-                      href={productUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Gå till produkt
-                    </ProductLink>
-                  </ProductContentWrapper>
+                  <ProductTitle>{productName}</ProductTitle>
+                  <ProductPrice>{productPrice}</ProductPrice>
+                  <ProductStatus inStock={productInStock}>
+                    {productInStock ? "Finns i lager" : "Slut"}
+                  </ProductStatus>
+                  <ProductLink
+                    href={productUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Gå till produkt
+                  </ProductLink>
                 </ProductContainer>
               </ListItem>
             )
